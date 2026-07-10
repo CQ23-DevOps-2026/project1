93462ba1d839fe429897323f5f8625a31752b99e
tranquocvy <quocvy23072005@gmail.com>
2026-07-10 22:44:47 +0700

    payment controller

--- payment/src/main/java/com/yas/payment/controller/PaymentController.java
+++ payment/src/main/java/com/yas/payment/controller/PaymentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+// payment controller
 @RestController
 @RequiredArgsConstructor
 public class PaymentController {
