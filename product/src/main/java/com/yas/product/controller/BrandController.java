d3f2a02fea0289fe2f360a563379553df7fac363
tranquocvy <quocvy23072005@gmail.com>
2026-07-10 22:54:17 +0700

    test payment coverage

--- product/src/main/java/com/yas/product/controller/BrandController.java
+++ product/src/main/java/com/yas/product/controller/BrandController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
+// check coverage payment
 @RestController
 public class BrandController {
     private static final Logger log = LoggerFactory.getLogger(BrandController.class);
